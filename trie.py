86a53ecd9e11a8554363dadac741286e77f63e18
Iffah Faizal <iffah.faizal@fdmgroup.com>
2024-01-30 16:41:32 +0800

    Initial Commit

---
+++ trie.py
@@ -0,0 +1,5 @@
+from trienode import TrieNode
+
+class Trie:
+    def __init__(self) -> None:
+        pass
---
+++ wordfinder.py
@@ -0,0 +1,6 @@
+from trie import Trie
+import sys
+
+class WordFinder:
+    def __init__(self):
+        pass
